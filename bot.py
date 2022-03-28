1cfd947027adc85c99b6c45d8d1aa60600a75291
Mattia Procopio <promat85@gmail.com>
2022-03-28 17:02:30 +0200

    Add more options

--- bot.py
+++ bot.py
@@ -1,21 +1,47 @@
+import argparse
+import datetime
 import json
 import os
 import random
+from datetime import datetime as dt
 
 import requests
 import tweepy
 
-hashtags = [
+troll_hashtags = [
     "istandwithrussia",
     "istandwithputin",
 ]
 
-answers = [
+love_hashtags = [
+    "istandwithukraine",
+    "fuckputin",
+]
+
+love_answers = [
+    "💙💛",
+    "🇺🇦",
+    "I ❤️ 🇺🇦",
+    "#IStandWithUkraine",
+    "#FuckPutin ❤️ 🇺🇦",
+    "слава Україні",
+]
+
+troll_answers = [
     "🇺🇦",
     "#istandwithukraine",
     "#putinzbrodniarzwojenny",
-    "freeukraine",
+    "free Ukraine",
+    "#UkraineWillRemainFree",
+    "#BlueYellowFreedom 🇺🇦",
     "nie strasz nie strasz, bo się zesrasz",
+    "#putinwarcriminal",
+    "#russiaIsTheOnlyNazi",
+    "#Srussia",
+    "#handsOffUkraine",
+    "#fuckPutin",
+    "#ZisTheNewSvastika",
+    "слава Україні",
 ]
 
 
@@ -24,7 +50,7 @@ CONSUMER_KEY = os.environ["CONSUMER_KEY"]
 CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
 ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
 ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
-EXCHANGE_URL = os.environ["EXCHANGE_API"]
+EXCHANGE_URL = os.environ["EXCHANGE_URL"]
 
 
 class TwitterBot:
@@ -39,13 +65,14 @@ class TwitterBot:
             access_token_secret=ACCESS_TOKEN_SECRET,
         )
 
-    def get_tweets(self):
-        joined_hashtags = ",".join(hashtags)
+    def get_tweets(self, hashtags):
+        joined_hashtags = "(" + " OR ".join(hashtags) + ")" + " -from:FuckPutinBot"
+        print(joined_hashtags)
         return self.client.search_recent_tweets(
             joined_hashtags,
             max_results=100,
-            start_time="2022-03-23T00:00:00Z",
-            end_time="2022-03-23T23:59:59Z",
+            start_time=dt.utcnow() - datetime.timedelta(hours=1),
+            end_time=dt.utcnow() - datetime.timedelta(minutes=10),
         )
 
     def _check_exchange(self):
@@ -53,33 +80,53 @@ class TwitterBot:
         return float(response.json()['rates']['RUB'])
 
     def calculate_rub_weight(self):
-        coins = self._check_exchange() // 10
-        return coins * 5.63
+        coins = self._check_exchange() / 10
+        return round(coins * 5.63, 2)
 
-    def reply_to_tweet(self, tweet_id):
-        self.client.create_tweet(
-            in_reply_to_tweet_id=tweet_id,
-            text=random.choice(answers),
-        )
+    def reply_to_tweet(self, tweet_id, answers):
+        try:
+            self.client.create_tweet(
+                in_reply_to_tweet_id=tweet_id,
+                text=random.choice(answers),
+            )
+        except tweepy.errors.Forbidden:
+            print(f"Couldn't reply to tweet {tweet_id}, forbidden")
 
-    def answer(self, tweets):
-        just_seen = {}
+    def answer(self, tweets, answers):
+        if tweets.data:
+            with open("seen.json", "r+") as f:
+                already_seen_tweets = json.loads(f.read())
+                f.seek(0)
 
-        with open("seen.json", "r+") as f:
-            already_seen_tweets = json.loads(f.read())
-            f.seek(0)
+                for tweet in tweets.data:
+                    if tweet.id not in already_seen_tweets:
+                        print(tweet)
+                        self.reply_to_tweet(tweet.id, answers)
+                        already_seen_tweets[tweet.id] = "DONE"
 
-            for tweet in tweets.data:
-                if tweet.id not in already_seen_tweets:
-                    print(tweet)
-                    self.reply_to_tweet(tweet.id)
-                    just_seen[tweet.id] = "DONE"
-
-            f.write(json.dumps(already_seen_tweets))
+                f.write(json.dumps(already_seen_tweets))
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--troll', help='Start trolling', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--love', help='Start trolling', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--tweet-exchange-weight',
+                        help='Tweet how many grams of rubles is worth 1$',
+                        action=argparse.BooleanOptionalAction)
+
+    args = parser.parse_args()
     bot = TwitterBot()
-    tweets = bot.get_tweets()
-    bot.answer(tweets)
-    #bot.client.create_tweet(text=f"1 USD weights {bot.calculate_rub_weight()}g of RUB")
+
+    if args.troll:
+        tweets = bot.get_tweets(troll_hashtags)
+        bot.answer(tweets, troll_answers)
+
+    if args.love:
+        tweets = bot.get_tweets(love_hashtags)
+        bot.answer(tweets, love_answers)
+
+    if args.tweet_exchange_weight:
+        bot.client.create_tweet(
+            text=f"1 USD weights {bot.calculate_rub_weight()}g of RUB, on {dt.now().isoformat()}"
+        )
